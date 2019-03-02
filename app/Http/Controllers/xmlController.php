2498b66a00572a64601b0a3fd873fdb00d35d05b
asgarimehdi <mehdi.asgari@gmail.com>
2019-03-03 01:28:52 +0330

    xml reading from behroo done

---
+++ app/Http/Controllers/xmlController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Orchestra\Parser\Xml\Facade as XmlParser;
+
+class xmlController extends Controller
+{
+    //
+    public function index(){
+        $xml = XmlParser::load('https://behroo165.com/product-sitemap1.xml');
+
+        $url = $xml->parse([
+            'urls' => ['uses' => 'url[loc,lastmod]'],
+        ]);
+
+        print_r( $url);
+    }
+}
